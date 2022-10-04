ee1872db239ebf68c39149c1bfbce27b8275ee15
oscar <oscar.chevalier@epita.fr>
2022-10-04 20:49:24 +0200

    solver: beggining of pretty_print, because pretty is better.

---
+++ src/solver/pretty_print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* pretty_print(grid): print the grid for the debug
+ *
+ *param grid: a matrix 9 times 9
+*/
+void pretty_print(int[9][9] grid)
+{
+    for(size_t i = 0; i<0;i++)
+        {
+            for
+        }
+}
