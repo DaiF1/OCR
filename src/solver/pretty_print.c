efc298ddf36aeea2fdbf926aea138ed3d01e8ce5
oscar-chevalier <oscar.m.chevalier@gmail.com>
2022-10-04 21:13:09 +0200

    solver: pretty_print.c finished but not tested shrug

--- src/solver/pretty_print.c
+++ src/solver/pretty_print.c
@@ -4,10 +4,22 @@
  *
  *param grid: a matrix 9 times 9
 */
-void pretty_print(int[9][9] grid)
+void pretty_print(int grid[9][9])
 {
-    for(size_t i = 0; i<0;i++)
+    for(size_t i = 0; i < 9; i++)
         {
-            for
+            for(size_t j = 0; j < 8; j++)
+            {
+                printf("%i", grid[i][j]);
+                if(j % 3 == 0)
+                    printf(" | ");
+                else
+                    printf(" ");
+            }
+            printf("%i\n", grid[i][8]);
+            if(i % 3 == 0)
+                printf("------+-------+------\n");
+            else
+                printf("      |       |\n");
         }
 }
