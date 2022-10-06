fec56d58945c6a60b5aa4900706bb1c389ab80fe
DaiF <julie.fiadino@gmail.com>
2022-10-06 12:34:03 +0200

    Add utils.h to main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,16 @@
+/*
+ * main.c in src/
+ * 
+ * Made by julie.fiadino
+ *
+ * OCR main file
+ *
+ * Started on  06/10 julie.fiadino
+ * Last Update 06/10 julie.fiadino
+*/
+#include "utils.h"
+
 int main()
 {
-  return 0;
+    return 0;
 }
