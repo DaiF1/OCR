ebeb63c3d74ae8923937647781bef0aabedb2bd2
DaiF <julie.fiadino@gmail.com>
2022-10-08 17:58:11 +0200

    Image loading test

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,22 @@
+/*
+ * test.c in tests/
+ *
+ * Made by julie.fiadino
+ *
+ * Main executable for unit testing
+ *
+ * Started on  06/10 julie.fiadino
+ * Last Update 08/10 julie.fiadino
+*/
+
 #include <stdlib.h>
+#include <assert.h>
+#include "loader.h"
+
 
 int main()
 {
-  return EXIT_SUCCESS; 
+    t_image img = load_img("img/000.png");
+    assert(img.pixels);
+    return EXIT_SUCCESS; 
 }
