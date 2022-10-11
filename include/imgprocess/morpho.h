8dfc9f41b523c3e5082a0ae748a39acc4003c691
DaiF <julie.fiadino@gmail.com>
2022-10-11 12:11:32 +0200

    morpho: represent matrices as lists

--- include/imgprocess/morpho.h
+++ include/imgprocess/morpho.h
@@ -5,7 +5,7 @@
  * File for morphological operations
  *
  * Started on  08/10 julie.fiadino
- * Last Update 09/10 julie.fiadino
+ * Last Update 11/10 julie.fiadino
 */
 
 #pragma once
@@ -17,33 +17,39 @@
  * circle_element(r): Build structuring element for morphological operations
  *
  * param r: radius of the circle
- * return: a matrix of size (2r + 1) * (2r + 1)
+ * return: a matrix of size (2r + 1) * (2r + 1) (represented as a list)
 */
-int32 **circle_element(const int32 r);
+int32 *circle_element(const size_t r);
 
 /*
  * morpho_erosion(img): Perform erosion on given image
  *
  * param img: source image
- * param s_el: structuring element (matrix)
+ * param s_el: structuring element (matrix as list)
+ * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_erosion(const t_image *img, const int8 **s_el);
+t_image *morpho_erosion(const t_image *img, const int32 *s_el,
+        const size_t len);
 
 /*
  * morpho_erosion(img): Perform dilationon given image
  *
  * param img: source image
- * param s_el: structuring element (matrix)
+ * param s_el: structuring element (matrix as list)
+ * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_dilation(const t_image *img, const int8 **s_el);
+t_image *morpho_dilation(const t_image *img, const int32 *s_el,
+        const size_t len);
 
 /*
  * morpho_erosion(img): Perform closing on given image
  *
  * param img: source image
- * param s_el: structuring element (matrix)
+ * param s_el: structuring element (matrix as list)
+ * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_closing(const t_image *img, const int8 **s_el);
+t_image *morpho_closing(const t_image *img, const int32 *s_el,
+        const size_t len);
