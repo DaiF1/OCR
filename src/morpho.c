fe9ad9887674ddcda7d0b5246745dae31585cdd9
DaiF <julie.fiadino@gmail.com>
2022-10-11 12:49:06 +0200

    Fix circle_element function

--- src/morpho.c
+++ src/morpho.c
@@ -5,26 +5,25 @@
  * Morpho.h Implementation
  *
  * Started on  08/10 julie.fiadino
- * Last Update 09/10 julie.fiadino
+ * Last Update 11/10 julie.fiadino
 */
 
 #include "morpho.h"
 
-int32 **circle_element(const int32 r)
+int32 *circle_element(const size_t r)
 {
     size_t len = 2 * r + 1;
-    int32 **result = malloc(sizeof(int32 *) * len);
+    int32 *result = malloc(sizeof(int32) * len * len);
 
     for (size_t y = 0; y < len; y++)
     {
-        *result = malloc(sizeof(int32) * len);
         for (size_t x = 0; x < len; x++)
         {
             int32 dist = (x - r) * (x - r) + (y - r) * (y - r);
-            if (dist < (r * r))
-                result[y][x] = 1;
+            if (dist <= (int32)(r * r))
+                result[y * len + x] = 1;
             else
-                result[y][x] = 0;
+                result[y * len + x] = 0;
         }
     }
 
--- tests/test.c
+++ tests/test.c
@@ -6,7 +6,7 @@
  * Main executable for unit testing
  *
  * Started on  06/10 julie.fiadino
- * Last Update 09/10 julie.fiadino
+ * Last Update 11/10 julie.fiadino
 */
 
 #include <stddef.h>
@@ -19,15 +19,15 @@
  *
  * param m1: a square matrix of size len * len
  * param m2: a square matrix of size len * len
- * param len: size of the square matrices
+ * param len: number of ligns of the square matrix
 */
-bool compare_matrices(int32 **m1, int32 **m2, size_t len)
+bool compare_matrices(int32 *m1, int32 *m2, size_t len)
 {
     for (size_t y = 0; y < len; y++)
     {
          for (size_t x = 0; x < len; x++)
          {
-             if (m1[y][x] != m2[y][x])
+             if (m1[y * len + x] != m2[y * len + x])
                  return false;
          }
     }
@@ -44,39 +44,25 @@ int main()
 
     // Morpho operations debug
     {
-        int32 c[5][5] = {
-            {0, 0, 1, 0, 0},
-            {0, 1, 1, 1, 0},
-            {1, 1, 1, 1, 1},
-            {0, 1, 1, 1, 0},
-            {0, 0, 1, 0, 0},
+        int32 c[] = {
+            0, 0, 1, 0, 0,
+            0, 1, 1, 1, 0,
+            1, 1, 1, 1, 1,
+            0, 1, 1, 1, 0,
+            0, 0, 1, 0, 0,
         };
 
-        int32 **tmp = malloc(sizeof(int32) * 5 * 5);
-        for (size_t i = 0; i < 5; i++)
-            *tmp = c[i];
-
-        int32 **c2 = circle_element(2);
-        assert(compare_matrices(tmp, c2, 5));
-        free(tmp);
-        free(c2);
+        assert(compare_matrices(c, circle_element(2), 5));
     }
 
     {
-        int32 c[3][3] = {
-            {0, 1, 0},
-            {1, 1, 1},
-            {0, 1, 0},
+        int32 c[] = {
+            0, 1, 0,
+            1, 1, 1,
+            0, 1, 0,
         };
 
-        int32 **tmp = malloc(sizeof(int32) * 3 * 3);
-        for (size_t i = 0; i < 5; i++)
-            *tmp = c[i];
-
-        int32 **c2 = circle_element(1);
-        assert(compare_matrices(tmp, c2, 3));
-        free(tmp);
-        free(c2);
+        assert(compare_matrices(c, circle_element(1), 3));
     }
 
     return EXIT_SUCCESS;
