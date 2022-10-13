f1aec905e79dcd8d954605b9fece9b7929faf218
DaiF <julie.fiadino@gmail.com>
2022-10-14 01:18:25 +0200

    Modify morpho operations return type

--- include/imgprocess/morpho.h
+++ include/imgprocess/morpho.h
@@ -5,7 +5,7 @@
  * File for morphological operations
  *
  * Started on  08/10 julie.fiadino
- * Last Update 11/10 julie.fiadino
+ * Last Update 13/10 julie.fiadino
 */
 
 #pragma once
@@ -16,40 +16,44 @@
 /*
  * circle_element(r): Build structuring element for morphological operations
  *
+ * param dest: destination matrix
  * param r: radius of the circle
  * return: a matrix of size (2r + 1) * (2r + 1) (represented as a list)
 */
-int32 *circle_element(const size_t r);
+void circle_element(int32 *dest, const size_t r);
 
 /*
- * morpho_erosion(img): Perform erosion on given image
+ * morpho_erosion(src, dest, s_el, len): Perform erosion on given image
  *
- * param img: source image
+ * param src: source image
+ * param dest: destination image
  * param s_el: structuring element (matrix as list)
  * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_erosion(const t_image *img, const int32 *s_el,
-        const size_t len);
+void morpho_erosion(const t_image *src, const t_image *dest,
+        const int32 *s_el, const size_t len);
 
 /*
- * morpho_erosion(img): Perform dilationon given image
+ * morpho_erosion(src, dest, s_el, len): Perform dilationon given image
  *
- * param img: source image
+ * param src: source image
+ * param dest: destination image
  * param s_el: structuring element (matrix as list)
  * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_dilation(const t_image *img, const int32 *s_el,
-        const size_t len);
+void morpho_dilation(const t_image *src, const t_image *dest,
+        const int32 *s_el, const size_t len);
 
 /*
- * morpho_erosion(img): Perform closing on given image
+ * morpho_erosion(src, dest, s_el, len): Perform closing on given image
  *
- * param img: source image
+ * param src: source image
+ * param dest: destination image
  * param s_el: structuring element (matrix as list)
  * param len: number of ligns in structuring element
  * return: the image after operation
 */
-t_image *morpho_closing(const t_image *img, const int32 *s_el,
-        const size_t len);
+void morpho_closing(const t_image *src, const t_image *dest,
+        const int32 *s_el, const size_t len);
