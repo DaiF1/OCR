1ca580410789ad2e7498afc8dbf1b6db5b2ce47b
oscar-chevalier <oscar.m.chevalier@gmail.com>
2022-10-15 17:56:49 +0200

    solver: maybe finished!

--- src/solver/solver.c
+++ src/solver/solver.c
@@ -6,13 +6,17 @@
  * the main file of the solver
  *
  * Started on 10/10 oscar.chevalier
- * Last update 11/10 oscar.chevalier
+ * Last update 15/10 oscar.chevalier
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "recursive_solver.h"
 #include "count_remaining_cases.h"
 #include "update_possibilities.h"
+#include "reader.h"
+#include "writer.h"
 
 int solver(int grill[9][9], int debug)
 {
@@ -22,4 +26,31 @@ int solver(int grill[9][9], int debug)
     if(debug)
         printf("solver:remaining_cases: %i\n", n);
     return recursive_solver(grill, 0, 0, possibilities, n, debug);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2)
+    {
+        int grill[9][9] =
+            {
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1},
+            };
+        int debug = 0;
+        read_sudoku(grill, argv[1], debug);
+        solver(grill, debug);
+        char name[] = ".result";
+        strcat(argv[1], name);
+        write_sudoku(grill, name, debug);
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
 }
