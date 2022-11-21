a4f1018bac292ca022597384ce29a842d990decf
DaiF <julie.fiadino@gmail.com>
2022-11-21 18:02:33 +0100

    gtk: bind image opener

--- src/main.c
+++ src/main.c
@@ -6,11 +6,32 @@
  * OCR main file
  *
  * Started on  06/10 julie.fiadino
- * Last Update 06/10 julie.fiadino
+ * Last Update 21/10 julie.fiadino
 */
 #include <gtk/gtk.h>
 
 #include "utils.h"
+
+typedef struct UI
+{
+    GtkWindow               *window;
+    GtkImage                *s_image;
+    GtkFileChooserButton    *file_chooser;
+    GtkToolButton           *save_button;
+    GtkToolButton           *solve_button;
+    GtkToolButton           *step_button;
+    GtkToolButton           *quit_button;
+} UI;
+
+void file_set(GtkFileChooserButton *button, gpointer user_data)
+{
+    UI *ui = user_data;
+
+    gchar *filename = gtk_file_chooser_get_filename(
+            GTK_FILE_CHOOSER(button));
+
+    gtk_image_set_from_file(ui->s_image, filename);
+}
 
 int main()
 {
@@ -26,10 +47,33 @@ int main()
         return 1;
     }
 
-    GtkWindow* window =
+    GtkWindow *window =
         GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.ocr"));
+    GtkImage *sudoku_image =
+        GTK_IMAGE(gtk_builder_get_object(builder, "_Sudoku"));
+    GtkFileChooserButton *open_button =
+        GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "_Open"));
+    GtkToolButton *save_button =
+        GTK_TOOL_BUTTON(gtk_builder_get_object(builder, "_Save"));
+    GtkToolButton *solve_button =
+        GTK_TOOL_BUTTON(gtk_builder_get_object(builder, "_Solve"));
+    GtkToolButton *step_button =
+        GTK_TOOL_BUTTON(gtk_builder_get_object(builder, "_Solve_step"));
+    GtkToolButton *quit_button =
+        GTK_TOOL_BUTTON(gtk_builder_get_object(builder, "_Quit"));
+
+    UI ui = {
+        .window = window,
+        .s_image = sudoku_image,
+        .file_chooser = open_button,
+        .save_button = save_button,
+        .solve_button = solve_button,
+        .step_button = step_button,
+        .quit_button = quit_button
+    };
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    g_signal_connect(open_button, "file-set", G_CALLBACK(file_set), &ui);
 
     gtk_main();
 
