806d67f69b567a782502ad96e50f5c10a2ef651e
DaiF <julie.fiadino@gmail.com>
2022-11-27 00:50:02 +0100

    interface: bind solve button

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 #include <gtk/gtk.h>
 
 #include "utils.h"
+#include "imgprocess.h"
 
 typedef struct UI
 {
@@ -23,6 +24,21 @@ typedef struct UI
     GtkToolButton           *quit_button;
 } UI;
 
+typedef struct Progress
+{
+    GtkDialog       *dialog;
+    GtkProgressBar  *bar;
+} Progress;
+
+gpointer thread_progress(gpointer user_data)
+{
+    Progress *progress = user_data;
+    
+    gtk_dialog_run(progress->dialog);
+
+    return NULL;
+}
+
 void file_set(GtkFileChooserButton *button, gpointer user_data)
 {
     UI *ui = user_data;
@@ -32,6 +48,48 @@ void file_set(GtkFileChooserButton *button, gpointer user_data)
 
     gtk_image_clear(ui->s_image);
     gtk_image_set_from_file(ui->s_image, filename);
+}
+
+void on_solve(GtkToolButton *button, gpointer user_data)
+{
+    UI *ui = user_data;
+
+
+    GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+    GtkDialog *dialog = GTK_DIALOG(gtk_message_dialog_new(ui->window,
+            flags,
+            GTK_MESSAGE_ERROR,
+            GTK_BUTTONS_CLOSE,
+            "Solving Sudoku..."));
+
+    GtkProgressBar *progress = GTK_PROGRESS_BAR(gtk_progress_bar_new());
+    gtk_widget_set_parent_window(GTK_WIDGET(progress), GDK_WINDOW(dialog));
+    gtk_widget_show(GTK_WIDGET(progress));
+ 
+    Progress bar = {dialog, progress};
+
+    g_thread_new("dialog", thread_progress, &bar);
+
+    GdkPixbuf *pixbuf = gtk_image_get_pixbuf(ui->s_image);
+    pixbuf = gdk_pixbuf_add_alpha(pixbuf, FALSE, 0, 0, 0);
+
+    t_image img = {0};
+    img.pixels = (uint32 *)gdk_pixbuf_get_pixels(pixbuf);
+    img.width = (int32)gdk_pixbuf_get_width(pixbuf);
+    img.height = (int32)gdk_pixbuf_get_height(pixbuf);
+
+    g_print("image collected!\n");
+
+    gray_scale(&img);
+    adjust_image(&img, 5);
+    black_and_white(&img);
+
+    g_print("image converted!\n");
+
+    DEBUG_display_image(&img);
+
+    gtk_widget_destroy(GTK_WIDGET(dialog));
+    gtk_widget_destroy(GTK_WIDGET(progress));
 }
 
 int main()
@@ -75,6 +133,7 @@ int main()
 
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(open_button, "file-set", G_CALLBACK(file_set), &ui);
+    g_signal_connect(solve_button, "clicked", G_CALLBACK(on_solve), &ui);
 
     gtk_main();
 
