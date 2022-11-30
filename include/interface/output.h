bf0fd9d604ecf65d401e0e892d920c8747b75864
DaiF <julie.fiadino@gmail.com>
2022-11-30 23:54:24 +0100

    interface: output image file

---
+++ include/interface/output.h
@@ -0,0 +1,30 @@
+/*
+ * output.h in include/interface
+ *
+ * Made by julie.fiadino
+ *
+ * Interface image output utility
+ *
+ * Started on  29/11 julie.fiadino
+ * Last Update 29/11 julie.fiadino
+*/
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "utils.h"
+#include "loader.h"
+
+typedef struct s_color
+{
+    uint8 r;
+    uint8 g;
+    uint8 b;
+} t_color;
+
+// Prints a number on a given surface
+void print_number(SDL_Surface *surface, char *text, int x, int y, t_color color);
+
+// Generate output image
+uint32 *generate_output(char *grid);
---
+++ src/output.c
@@ -0,0 +1,21 @@
+/*
+ * output.c in src/
+ *
+ * Made by julie.fiadino
+ *
+ * Interface image output utility
+ *
+ * Started on  29/11 julie.fiadino
+ * Last Update 29/11 julie.fiadino
+*/
+#include "interface/output.h"
+
+void print_number(SDL_Surface *surface, char *text,
+        int x, int y, t_color color)
+{
+}
+
+uint32 *generate_output(char *grid)
+{
+    return 0;
+}
